a74fcb229f49158baaf94f80c24ba8a42f432b96
JulienConan <72304768+JulienConan@users.noreply.github.com>
2021-01-09 17:08:11 +0100

    move data_base

--- models/constants.py
+++ models/constants.py
@@ -6,6 +6,6 @@ TOURNAMENT_PLAYERS_NB = 8
 # Default tournament's number of round
 ROUNDS_NB = 4
 # Default path's players database
-P_DATABASE = 'models/datas_base/players_file.json'
+P_DATABASE = 'datas_base/players_file.json'
 # Default path's tournaments database
-T_DATABASE = 'models/datas_base/tournament_file.json'
+T_DATABASE = 'datas_base/tournament_file.json'
